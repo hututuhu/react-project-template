2c57166e049be4bc6739b12f69cacaf4c9a77d4a
hushanqing <37233828+hututuhu@users.noreply.github.com>
2021-03-17 14:51:58 +0800

    Add files via upload

---
+++ component-react/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+/*
+ * 组件注释
+ */
+const ComponentName = () => {
+	return <div className={styles.container}>我是一个React示例</div>
+}
+
+export default ComponentName;
